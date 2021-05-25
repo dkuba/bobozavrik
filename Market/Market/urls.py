44ea08818ab1fc4e965597d37d1014e47795c42b
Vadym Drok <drokvadim@gmail.com>
2021-05-25 12:17:25 +0300

    add ckeditor

--- Market/Market/urls.py
+++ Market/Market/urls.py
@@ -20,4 +20,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('main.urls')),
     path('pages/', include('django.contrib.flatpages.urls')),
-]
+    path('ckeditor/', include('ckeditor_uploader.urls')),
+] 
--- Market/main/admin.py
+++ Market/main/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from django.contrib.flatpages.models import FlatPage
+from django.db import models
+from ckeditor.fields import RichTextField
+from ckeditor_uploader.fields import RichTextUploadingField
 
-# Register your models here.
+
+admin.site.unregister(FlatPage)
+
+
+class FlatPageAdmin(admin.ModelAdmin):
+    content = RichTextUploadingField()
+    class Meta:
+        model = FlatPage
+
+admin.site.register(FlatPage, FlatPageAdmin)
+

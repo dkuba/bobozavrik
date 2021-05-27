f1c4dfba38928c0968055aca6201cd6bbb877975
Vadym Drok <drokvadim@gmail.com>
2021-05-27 18:01:50 +0300

    dop CKEditor

--- Market/main/admin.py
+++ Market/main/admin.py
@@ -1,20 +1,22 @@
 from django.contrib import admin
+
 from django.contrib.flatpages.models import FlatPage
+from django.contrib.flatpages.admin import FlatPageAdmin
+
 from django.db import models
-from ckeditor.fields import RichTextField
-from ckeditor_uploader.fields import RichTextUploadingField
+
+from ckeditor.widgets import CKEditorWidget
+
 from .models import *
 
 
 admin.site.unregister(FlatPage)
 
+class FlatPageCustom(FlatPageAdmin):
+    formfield_overrides = {models.TextField: {'widget': CKEditorWidget}}
 
-class FlatPageAdmin(admin.ModelAdmin):
-    content = RichTextUploadingField()
-    class Meta:
-        model = FlatPage
+admin.site.register(FlatPage, FlatPageCustom)
 
-admin.site.register(FlatPage, FlatPageAdmin)
 
 class CategoryAdmin(admin.ModelAdmin):
     class Meta:
@@ -22,17 +24,20 @@ class CategoryAdmin(admin.ModelAdmin):
 
 admin.site.register(Category, CategoryAdmin)
 
+
 class AdAdmin(admin.ModelAdmin):
     class Meta:
         model = Ad
 
 admin.site.register(Ad, AdAdmin)
 
+
 class TagAdmin(admin.ModelAdmin):
     class Meta:
         model = Tag
 
 admin.site.register(Tag, TagAdmin)
+
 
 class SellerAdmin(admin.ModelAdmin):
     class Meta:

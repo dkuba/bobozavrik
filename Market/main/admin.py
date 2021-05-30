1e63bbfb699721f45b1d1e85abb2ee964cb4018c
Vadym Drok <drokvadim@gmail.com>
2021-05-30 20:30:13 +0300

    add new model/ add proxy model

--- Market/main/admin.py
+++ Market/main/admin.py
@@ -11,10 +11,8 @@ from .models import *
 
 
 admin.site.unregister(FlatPage)
-
 class FlatPageCustom(FlatPageAdmin):
     formfield_overrides = {models.TextField: {'widget': CKEditorWidget}}
-
 admin.site.register(FlatPage, FlatPageCustom)
 
 
@@ -22,26 +20,54 @@ class CategoryAdmin(admin.ModelAdmin):
     prepopulated_fields = {"slug": ("title",)}
     class Meta:
         model = Category
-
 admin.site.register(Category, CategoryAdmin)
 
 
 class AdAdmin(admin.ModelAdmin):
     class Meta:
         model = Ad
-
 admin.site.register(Ad, AdAdmin)
+
+class AdArchiveAdmin(admin.ModelAdmin):
+    class Meta:
+        model = AdArchive
+admin.site.register(AdArchive, AdArchiveAdmin)
+
+
 
 
 class TagAdmin(admin.ModelAdmin):
     class Meta:
         model = Tag
-
 admin.site.register(Tag, TagAdmin)
 
 
 class SellerAdmin(admin.ModelAdmin):
     class Meta:
         model = Seller
-
 admin.site.register(Seller, SellerAdmin)
+
+
+
+class StuffAdmin(admin.ModelAdmin):
+    class Meta:
+        model = Stuff
+admin.site.register(Stuff, StuffAdmin)
+
+
+class TypeFuelAdmin(admin.ModelAdmin):
+    class Meta:
+        model = TypeFuel
+admin.site.register(TypeFuel, TypeFuelAdmin)
+
+
+class CarAdmin(admin.ModelAdmin):
+    class Meta:
+        model = Car
+admin.site.register(Car, CarAdmin)
+
+
+class ServicesAdmin(admin.ModelAdmin):
+    class Meta:
+        model = Services
+admin.site.register(Services, ServicesAdmin)
--- Market/main/models.py
+++ Market/main/models.py
@@ -1,9 +1,9 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-from django.utils.text import slugify
-import random
-import string
+# from django.utils.text import slugify
+# import random
+# import string
 
 # def random_string_generator(size=10, chars=string.ascii_lowercase + string.digits):
 #     return ''.join(random.choice(chars) for _ in range(size))
@@ -14,7 +14,7 @@ class Category(models.Model):
     slug = models.SlugField(unique=True, verbose_name='Идентификатор')
 
     def __str__(self):
-        return 'Ad: %s' % self.title
+        return 'Category: %s' % self.title
     
     # def unique_slug_generator(instance, new_slug=None):
     #     """
@@ -40,6 +40,7 @@ class Category(models.Model):
 class Ad(models.Model):
     title = models.CharField(max_length=200)
     description = models.TextField()
+    price = models.PositiveIntegerField(default=1)
     
     category = models.ForeignKey(Category, blank=True, null=True, on_delete=models.CASCADE, default=None)
     
@@ -50,21 +51,67 @@ class Ad(models.Model):
         return 'Ad: %s' % self.title
 
 
+class AdArchive(Ad):
+    
+    class Meta:
+        proxy = True
+        ordering = ["created_date"]
+        
+
 class Tag(models.Model):
     title = models.CharField(max_length=200)
     
     def __str__(self):
-        return 'Ad: %s' % self.title
+        return 'Tag: %s' % self.title
 
 
-class Seller(models.Model):
-    name = models.ForeignKey(User, blank=True, null=True, on_delete=models.CASCADE, default=None)
-
+class Seller(User):
+    name = models.ForeignKey(User, blank=True, null=True, on_delete=models.CASCADE, default=None, related_name = "Seller")
     
     def __str__(self):
-        return 'Ad: %s' % self.title
+        return 'Seller: %s' % self.title
     
     def nmd_of_ads():
         nmb_ad = Ad.objects.count()
         return nmb_ad
+    
+
+class BaseAd(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    price = models.PositiveIntegerField(default=1)
+    created = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        abstract = True
+
+
+class Stuff(BaseAd):
+    is_new = models.BooleanField(default=True)
+    
+    def __str__(self):
+        return 'Stuff: %s' % self.title
+
+
+class TypeFuel(models.Model):
+    type_fuel = models.CharField(max_length=24, blank=True, null=True, default=None)
+    def __str__(self):
+        return 'TypeFuel: %s' % self.type_fuel
+       
+class Car(BaseAd):
+    engine_volume = models.DecimalField(max_digits=5, decimal_places=3, default=0)
+    type_fuel = models.ForeignKey(TypeFuel, on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return 'Car: %s' % self.title
+    
+    
+class Services(BaseAd):
+    duration = models.DateTimeField()
+    def __str__(self):
+        return 'Car: %s' % self.title
+    
+    
+
+    
 

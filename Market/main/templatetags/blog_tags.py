4267e25a84743e73b1c1fb1fc1774c102435defa
Vadym Drok <drokvadim@gmail.com>
2021-06-01 08:48:59 +0300

    add custom tag and filter

---
+++ Market/main/templatetags/blog_tags.py
@@ -0,0 +1,23 @@
+from django import template
+
+from django.template.defaultfilters import stringfilter
+
+import datetime
+from django.utils.timezone import utc
+
+register = template.Library()
+
+from ..models import Ad
+
+@register.simple_tag
+def current_time():
+    now = datetime.datetime.utcnow().replace(tzinfo=utc)
+    return now
+
+
+
+@register.filter
+@stringfilter
+def palindrome(string):
+    reversed_string = string[::-1]
+    return  reversed_string

de3e03b4af9b39e4b8e84ae7f7e58e549f75f9c2
Vadym Drok <drokvadim@gmail.com>
2021-06-12 15:08:46 +0300

    send email from new user (when registered)

--- Market/Market/settings.py
+++ Market/Market/settings.py
@@ -135,8 +135,16 @@ SOCIALACCOUNT_PROVIDERS = {
 }
 
 AUTH_PARAMS = ['access_type'] 
-DEFAULT_FROM_EMAIL = 'you@domain.com' 
-# EMAIL_BACKEND = venvl('DJANGO_EMAIL_BACKEND', default='django.core.mail.backends.console.EmailBackend')
+
+
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'drokvadim@gmail.com'
+EMAIL_HOST_PASSWORD = '***'
 
 SITE_ID = 2
 

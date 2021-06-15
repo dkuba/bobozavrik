09526b20e4d8e0aa72663b04c1f83dfdcabffe6e
Vadym Drok <drokvadim@gmail.com>
2021-06-15 08:18:45 +0300

    edit schedualer - filter for 7ays

--- Market/main/jobs_1.py
+++ Market/main/jobs_1.py
@@ -1,14 +1,17 @@
 from .models import *
+from django.utils.timezone import now
 
 
 def add_new_ad():
-    """Send email for new User about new ad"""
+    """Send email for new User about new ad for last 7 days"""
 
     cars = Car.objects.all()
     title_list=[]
+    today = now()
     for car in cars:
-        new_car = car.title.encode('utf-8')
-        title_list.append(new_car)
+        if (today.day - car.created.day) > 7:
+            new_car = car.title.encode('utf-8')
+            title_list.append(new_car)
     for item in Subscriber.objects.all():
         email_adress = item.email            
         data = {

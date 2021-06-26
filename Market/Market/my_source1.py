be2295f370d04e7d807811e51f95e0164f2de683
Vadym Drok <drokvadim@gmail.com>
2021-06-26 10:36:12 +0300

    add tests

---
+++ Market/Market/my_source1.py
@@ -0,0 +1,9 @@
+
+
+g_password = 'NewToyStory13'
+g_email = 'drokvadim@gmail.com'
+
+
+# Twilo
+my_ACCOUNT_SID = 'ACac2de25ba064bcb9d30eaa842e4651c2'
+my_AUTH_TOKEN = 'd9ec0056562b2a65a8b85bab2dce30c8'

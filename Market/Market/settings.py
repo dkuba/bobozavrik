b32b50df9ef12c3478a3edd67aee66f41f2fc0b3
Vadym Drok <drokvadim@gmail.com>
2021-07-06 15:23:22 +0300

    add PosgreSQL

--- Market/Market/settings.py
+++ Market/Market/settings.py
@@ -91,13 +91,17 @@ WSGI_APPLICATION = 'Market.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': 'pgdb',
+        'PORT': 5432,
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
@@ -154,7 +158,7 @@ EMAIL_USE_TLS = True
 EMAIL_HOST_USER = g_email
 EMAIL_HOST_PASSWORD = g_password
 
-SITE_ID = 3
+SITE_ID = 4
 
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'

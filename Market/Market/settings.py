9b4835c7f44868c9293e27a24c07ba88fdf8d676
Vadym Drok <drokvadim@gmail.com>
2021-07-08 08:37:47 +0300

    add logging with sentry

--- Market/Market/settings.py
+++ Market/Market/settings.py
@@ -90,6 +90,24 @@ TEMPLATES = [
 WSGI_APPLICATION = 'Market.wsgi.application'
 
 
+import sentry_sdk
+from sentry_sdk.integrations.django import DjangoIntegration
+
+sentry_sdk.init(
+    dsn="https://7f7c0f4d42644dbda90c68f0febaaa21@o914849.ingest.sentry.io/5854337",
+    integrations=[DjangoIntegration()],
+
+    # Set traces_sample_rate to 1.0 to capture 100%
+    # of transactions for performance monitoring.
+    # We recommend adjusting this value in production.
+    traces_sample_rate=1.0,
+
+    # If you wish to associate users to errors (assuming you are using
+    # django.contrib.auth) you may enable sending PII data.
+    send_default_pii=True
+)
+
+
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 

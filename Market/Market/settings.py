99be60b50ac8d47eea235fca75139eeea9f1d94b
Vadym Drok <drokvadim@gmail.com>
2021-07-17 10:59:50 +0300

    test

--- Market/Market/settings.py
+++ Market/Market/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-)gj!$$n)-q(s+y4#_g^p)fs1v4rgh4)!%r-j&2^9o)c5bh=nvz'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
